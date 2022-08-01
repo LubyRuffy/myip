64663600656e8733901a7d81ea2e12bd07faf5ba
Luby Ruffy <lubyruffy@gmail.com>
2022-08-01 14:27:30 +0800

    没有upstream不输出upstream_country

--- main.go
+++ main.go
@@ -261,9 +261,11 @@ func countryAction(w http.ResponseWriter, r *http.Request) {
 		if err == nil && record != nil {
 			result["country"] = record["country"].(map[string]interface{})["names"].(map[string]interface{})["en"].(string)
 		}
-		err = ipDb.Lookup(net.ParseIP(upstream), &record)
-		if err == nil && record != nil {
-			result["upstream_country"] = record["country"].(map[string]interface{})["names"].(map[string]interface{})["en"].(string)
+		if upstream != ip {
+			err = ipDb.Lookup(net.ParseIP(upstream), &record)
+			if err == nil && record != nil {
+				result["upstream_country"] = record["country"].(map[string]interface{})["names"].(map[string]interface{})["en"].(string)
+			}
 		}
 	}
 
