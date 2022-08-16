73b1f49b796374f1caebec6a3d30b13e8422a337
Luby Ruffy <lubyruffy@gmail.com>
2022-08-16 15:46:13 +0800

    增加autotls支持

--- update_myip_server.sh
+++ update_myip_server.sh
@@ -2,4 +2,4 @@
 # update
 go install github.com/LubyRuffy/myip@latest
 # restart
-sudo pkill myip | sudo nohup sh -c "`go env GOPATH`/bin/myip -addr :80" 1>myip_out.txt 2>myip_err.txt &
+sudo pkill myip | sudo nohup sh -c "`go env GOPATH`/bin/myip -addr :80 -autotls -subdomains ip.bmh.im" 1>myip_out.txt 2>myip_err.txt &
