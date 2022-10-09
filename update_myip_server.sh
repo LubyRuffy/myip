2ed23a51a8f5345327a9c291fd355253e964a639
Luby Ruffy <lubyruffy@gmail.com>
2022-10-09 14:56:21 +0800

    更新脚本

--- update_myip_server.sh
+++ update_myip_server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 # update
-go install github.com/LubyRuffy/myip@latest
+go install github.com/LubyRuffy/myip@main
 # restart
 sudo pkill myip | sudo nohup sh -c "`go env GOPATH`/bin/myip -addr :80 -autotls -subdomains ip.bmh.im" 1>myip_out.txt 2>myip_err.txt &
